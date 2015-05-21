3e1224dad83e60027d56e8ae4de6013f1ddc62f9
Yu Hoshino <yhoshino11@gmail.com>
2015-05-21 07:50:42 +0000

    Add Distribution information to setup.py.

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
-from setuptools import setup
-setup(name='guestbook')
+from setuptools import setup, find_packages
+setup(
+  name='guestbook',
+  version='1.0.0',
+  packages=find_packages(),
+  include_package_data=True,
+  install_requires=[
+    'Flask',
+  ],
+)
