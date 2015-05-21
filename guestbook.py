bb21a4165c1508559060d60c821c6cbc22f7b182
Yu Hoshino <yhoshino11@gmail.com>
2015-05-21 02:41:54 +0000

    Add URL for Post

--- guestbook.py
+++ guestbook.py
@@ -1,6 +1,12 @@
 # coding: utf-8
 import shelve
 
+from datetime import datetime
+
+from flask import Flask, request, render_template, redirect, escape, Markup
+
+application = Flask(__name__)
+
 DATA_FILE = 'guestbook.dat'
 
 def save_data(name, comment, create_at):
@@ -43,3 +49,37 @@ def load_data():
     greeting_list = database.get('greeting_list', [])
     database.close()
     return greeting_list
+
+
+@application.route('/')
+def index():
+    """Top Page
+    Show page with template.
+    """
+
+    # Load Posted data
+    greeting_list = load_data()
+
+    return render_template('index.html', greeting_list=greeting_list)
+
+
+@application.route('/post', methods=['POST'])
+def post():
+    """URL for Post
+    """
+
+    # Receive Posted data
+    name      = request.form.get('name')
+    comment   = request.form.get('comment')
+    create_at = datetime.now()
+
+    # Save data
+    save_data(name, comment, create_at)
+
+    # Redirect to top page after saving
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    # Execute with http://127.0.0.1:8000
+    application.run('127.0.0.1', 8000, debug=True)
